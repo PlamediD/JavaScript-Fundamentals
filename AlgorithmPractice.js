f541f83573ba2701d913bc732b406ded93ccb2e3
PlamediD <87151146+PlamediD@users.noreply.github.com>
2021-07-16 23:59:03 -0700

    Add files via upload

---
+++ AlgorithmPractice.js
@@ -0,0 +1,5 @@
+for(var num = 0; num < 20; num++){
+    if(j%2!=0){
+    console.log(num);
+    }
+}
---
+++ AlgorithmPractice2.js
@@ -0,0 +1,6 @@
+var n=1;
+for(n=1;n<6;n++){
+  var sum = (n * (n+1)) / 2;
+console.log("num"+n+":" +n);
+console.log("sum"+n+":"+sum);
+}
